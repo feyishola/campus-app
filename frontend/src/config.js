2a3ba501a825ad008c0d4d1a9dba1b70d6e139ee
feyishola <feyisholaologunebi@gmail.com>
2025-02-05 15:56:19 +0100

    changed localhost to the online url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,8 @@
 const envConfig = {
   REACT_APP_MAPBOX_TOKEN:
     "pk.eyJ1IjoiYXJhanRhIiwiYSI6ImNsMGRlMmQzZTA4NG0zZHBvcW1yYnhseDUifQ.Ks6svhgtp-klQ__3qQbS9A",
-  BASE_URL: "http://localhost:5000/api/v1/campusmap",
-  // BASE_URL: "https://campus-app-e9tp.onrender.com/api/v1/campusmap",
+  // BASE_URL: "http://localhost:5000/api/v1/campusmap",
+  BASE_URL: "https://campus-app-e9tp.onrender.com/api/v1/campusmap",
 };
 
 export default envConfig;
